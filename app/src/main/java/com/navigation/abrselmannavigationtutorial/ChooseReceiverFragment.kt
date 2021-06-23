e931ef575a1b0185e7ad80ee7317fe7f026d1982
Mohmed Salman Siddiqi <salmansiddique897@gmail.com>
2021-06-23 09:05:51 +0530

    1: First Basic as Master

---
+++ app/src/main/java/com/navigation/abrselmannavigationtutorial/ChooseReceiverFragment.kt
@@ -0,0 +1,9 @@
+package com.navigation.abrselmannavigationtutorial
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ChooseReceiverFragment : Fragment(R.layout.fragment_choose_receiver)
