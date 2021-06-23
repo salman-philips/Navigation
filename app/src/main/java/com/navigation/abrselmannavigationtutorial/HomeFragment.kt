75a80b49edb740d2c23786fb8298cf34a31628a2
Mohmed Salman Siddiqi <salmansiddique897@gmail.com>
2021-06-23 09:27:40 +0530

    2: Navigation from ids

--- app/src/main/java/com/navigation/abrselmannavigationtutorial/HomeFragment.kt
+++ app/src/main/java/com/navigation/abrselmannavigationtutorial/HomeFragment.kt
@@ -2,9 +2,18 @@ package com.navigation.abrselmannavigationtutorial
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val navigationController = findNavController()
+        super.onViewCreated(view, savedInstanceState)
+        btn_view_balance.setOnClickListener { navigationController.navigate(R.id.viewBalanceFragment) }
+        btn_transactions.setOnClickListener { navigationController.navigate(R.id.viewTransactionsFragment) }
+        btn_send_money.setOnClickListener { navigationController.navigate(R.id.chooseReceiverFragment) }
+    }
+}
