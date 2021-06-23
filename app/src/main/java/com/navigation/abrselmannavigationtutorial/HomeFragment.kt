18b02db9652e99644eb5abca66b4033c9ba761f3
Mohmed Salman Siddiqi <salmansiddique897@gmail.com>
2021-06-23 09:59:10 +0530

    5: Navigation using ids without actions defined but with animations using navOptions.Builder

--- app/src/main/java/com/navigation/abrselmannavigationtutorial/HomeFragment.kt
+++ app/src/main/java/com/navigation/abrselmannavigationtutorial/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.navigation.abrselmannavigationtutorial
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -12,8 +13,37 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val navigationController = findNavController()
         super.onViewCreated(view, savedInstanceState)
-        btn_view_balance.setOnClickListener { navigationController.navigate(R.id.viewBalanceFragment) }
-        btn_transactions.setOnClickListener { navigationController.navigate(R.id.viewTransactionsFragment) }
-        btn_send_money.setOnClickListener { navigationController.navigate(R.id.chooseReceiverFragment) }
+        btn_view_balance.setOnClickListener {
+
+            val navOptions= NavOptions.Builder()
+                .setEnterAnim(R.anim.slide_in_right)
+                .setExitAnim(R.anim.slide_out_left)
+                .setPopEnterAnim(R.anim.slide_out_right)
+                .setPopExitAnim(R.anim.slide_out_left)
+                .build()
+
+            navigationController.navigate(R.id.viewBalanceFragment,null,navOptions) }
+
+        btn_transactions.setOnClickListener {
+
+            val navOptions= NavOptions.Builder()
+                .setEnterAnim(R.anim.slide_in_right)
+                .setExitAnim(R.anim.slide_out_left)
+                .setPopEnterAnim(R.anim.slide_out_right)
+                .setPopExitAnim(R.anim.slide_out_left)
+                .build()
+
+            navigationController.navigate(R.id.viewTransactionsFragment,null,navOptions) }
+
+        btn_send_money.setOnClickListener {
+
+            val navOptions= NavOptions.Builder()
+                .setEnterAnim(R.anim.slide_in_right)
+                .setExitAnim(R.anim.slide_out_left)
+                .setPopEnterAnim(R.anim.slide_out_right)
+                .setPopExitAnim(R.anim.slide_out_left)
+                .build()
+
+            navigationController.navigate(R.id.chooseReceiverFragment,null,navOptions) }
     }
 }
