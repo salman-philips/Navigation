bd4819bb03d91b49d1d22e38e27ac76c812097d1
Mohmed Salman Siddiqi <salmansiddique897@gmail.com>
2021-06-23 09:48:33 +0530

    4: Navigation from actions obtained through the direction class generated from the source destination with slide animations

--- app/src/main/java/com/navigation/abrselmannavigationtutorial/HomeFragment.kt
+++ app/src/main/java/com/navigation/abrselmannavigationtutorial/HomeFragment.kt
@@ -12,8 +12,18 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val navigationController = findNavController()
         super.onViewCreated(view, savedInstanceState)
-        btn_view_balance.setOnClickListener { navigationController.navigate(R.id.action_homeFragment_to_viewBalanceFragment) }
-        btn_transactions.setOnClickListener { navigationController.navigate(R.id.action_homeFragment_to_viewTransactionsFragment) }
-        btn_send_money.setOnClickListener { navigationController.navigate(R.id.action_homeFragment_to_chooseReceiverFragment) }
+
+        btn_view_balance.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToViewBalanceFragment()
+            navigationController.navigate(action)
+        }
+        btn_transactions.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToViewTransactionsFragment()
+            navigationController.navigate(action)
+        }
+        btn_send_money.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToChooseReceiverFragment()
+            navigationController.navigate(action)
+        }
     }
 }
