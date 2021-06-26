08129f8e29c1e0d9628b9e4f244385d599f4a7b4
Mohmed Salman Siddiqi <salmansiddique897@gmail.com>
2021-06-26 20:51:38 +0530

    12 : set up toolbar

--- app/src/main/java/com/navigation/abrselmannavigationtutorial/MainActivity.kt
+++ app/src/main/java/com/navigation/abrselmannavigationtutorial/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navigationController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navigationController = navHostFragment.navController
